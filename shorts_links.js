9334a1dbfc2e4ca877525f6bc2da4b9fa296b51d
toukirsonalibank-sys <toukir.sonalibank@gmail.com>
2026-03-16 06:17:35 +0600

    Update shorts_links.js

--- shorts_links.js
+++ shorts_links.js
@@ -1,5 +1,3 @@
 window.SHORTS_LINKS = [
-  'https://youtube.com/shorts/6WZmsubof6k?si=uU9LMaVnHKgKTXqq',
-  'https://youtube.com/shorts/Ay6qLrxWyWs',
-  'https://youtube.com/shorts/ZXQahDFN-Nk?si=7RhhDkv_9VErh6U8'
+  'https://youtube.com/shorts/6WZmsubof6k?si=uU9LMaVnHKgKTXqq'
 ];
